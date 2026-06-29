3aeb8e7111ac4e3e0d6b6a932f4eb40ec18bc071
Anninhastar23 <camposannalivia127@gmail.com>
2026-06-29 00:29:23 -0300

    Criação do arquivo mysql.ts e configuração da conexão com o MySQL

---
+++ src/database/mysql.ts
@@ -0,0 +1,61 @@
+import mysql, {Connection, QueryError} from 'mysql2'
+import {ClienteRepository} from '../repositories/clienteRepository'
+import {VendedorRepository} from '../repositories/vendedorRepository'
+import {CarroRepository} from '../repositories/carroRepository'
+import {EstoqueRepository} from '../repositories/estoqueRepository'
+import {NotaFiscalRepository} from '../repositories/notaFiscalRepository'
+
+const dbConfig = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'mysql',
+    database: 'concessionaria'
+}
+
+const mysqlConnection: Connection = mysql.createConnection(dbConfig)
+
+mysqlConnection.connect((err) => {
+    if(err){
+        console.error('Erro ao conectar ao banco de dados:', err)
+        throw err
+    }
+    console.log('Conexão bem-sucedida com o banco de dados MySQL')
+})
+
+export function executarComandoSQL(query: string, valores: any[]): Promise<any>{
+    return new Promise<any>((resolve, reject) => {
+        mysqlConnection.query(query, valores, (err, resultado) => {
+            if (err) {
+                console.error('Erro ao executar a query.', err)
+                reject(err)
+            }
+            resolve(resultado)
+        })
+    })
+}
+
+export async function inicializarBanco(): Promise<void>{
+    console.log('Sincronizando schemas do banco de dados...')
+
+    const schemas = [
+        ClienteRepository.getCreateTableQuery(),
+        VendedorRepository.getCreateTableQuery(),
+        CarroRepository.getCreateTableQuery(),
+        EstoqueRepository.getCreateTableQuery(),
+        NotaFiscalRepository.getCreateTableQuery()
+    ]
+
+    try {
+        await executarComandoSQL(`USE ${dbConfig.database}`, [])
+        console.log(`Conectado ao schema: ${dbConfig.database}`)
+
+        for (const query of schemas) {
+            await executarComandoSQL(query, [])
+        }
+        console.log('Todos os repositórios foram inicializados com sucesso.')
+    } catch (err) {
+        console.error('Erro crítico na sincronização dos repositórios:', err)
+        process.exit(1)
+    }
+}
